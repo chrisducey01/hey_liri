3fd42e4ca00ef29fa2a29bb71358fd8b5a0a76f4
Christopher Ducey <chrisducey01@me.com>
2019-09-30 21:40:36 -0700

    Added basic funtionality to return results from spotify api call

--- liri.js
+++ liri.js
@@ -3,15 +3,45 @@ const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 
 
-const spotifyApi = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 
-spotifyApi.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-    data.tracks.items.forEach(item =>{
-        console.log(item);
+const action = process.argv[2];
+const lookupItem = process.argv[3];
+
+switch (action) {
+  case "spotify-this-song":
+    spotifySongSearch(spotify, lookupItem);
+    break;
+  default:
+    console.log(`Error: Unknown option specified on input: ${action}`);
+}
+
+function spotifySongSearch(spotify, song) {
+  spotify.search(
+    {
+      type: 'track',
+      query: song,
+      limit: 1
+    },
+    (err, data) => {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      const songResult = data.tracks.items[0];
+      const artistNames = songResult.artists.map(artist => {
+        return artist.name;
+      }).join(", ");
+
+      // Dump raw data to log
+      // console.log(data);
+      // console.log(data.tracks.items[0]);
+      // console.log(data.tracks.items[0].artists);
+
+      console.log(`Artist${artistNames.includes(",") ? "s" : ""}: ${artistNames}`);
+      console.log(`Song: ${songResult.name}`);
+      console.log(`Album: ${songResult.album.name}`);
+      songResult.preview_url ? console.log(`Listen to a Preview: ${songResult.preview_url}`)
+        : console.log(`Listen on Spotify: ${songResult.external_urls.spotify}`);
     });
-//   console.log(data); 
-  });
+}
